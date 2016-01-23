df2a2d6aa6d98d3183dd66204bb8db2b671e827e
Akerke <okapova.akerke@gmail.com>
2016-01-23 14:24:42 +0600

    lab2_2

---
+++ programmingTechnologies/lab2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F1();
+        }
+
+        private static void F1()
+        {
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.);
+            StreamReader sr = new StreamReader(fs);
+
+            string line = sr.ReadLine();
+            string[] arr = line.Split(' ');
+
+            int max = 0;
+            int min = 0;
+
+
+        }
+    }
+}
