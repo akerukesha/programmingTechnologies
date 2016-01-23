b1ebf2d14124c7426e4099157c34251f88527273
Akerke <okapova.akerke@gmail.com>
2016-01-23 14:41:10 +0600

    Comments added.

--- programmingTechnologies/lab2_2/Program.cs
+++ programmingTechnologies/lab2_2/Program.cs
@@ -13,19 +13,32 @@ namespace lab2_2
         {
             F1();
         }
-
+        //необходимо создать метод F1, который вызывается в Main
         private static void F1()
-        {
-            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.);
+        {   //считываем значения с input.txt
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
-
+            //значения считываются в виде строки, которую необходимо разбить
             string line = sr.ReadLine();
             string[] arr = line.Split(' ');
-
+            //задаются начальные значения для максимума и минимума
             int max = 0;
-            int min = 0;
+            int min = 1000;
+            //берется каждое значение массива
+            for (int i = 0; i < arr.Length; i++)
+            {   //конвертируется с типа string в тип integer
+                int currentNumber = int.Parse(arr[i]);
+                if (currentNumber > max)
+                {
+                    max = currentNumber;
+                }
+                else if (currentNumber < min)
+                {
+                    min = currentNumber;
+                }
+            }
 
-
+            Console.WriteLine("Maximum: " + max + "\nMinumum: " + min);
         }
     }
 }
